13ca94d0686ca5533029ff823fe5af11357dc891
busti <mbust@live.de>
2019-12-20 14:33:15 +0100

    update lockfile; format code

--- src/main.js
+++ src/main.js
@@ -14,9 +14,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faPlus, faCheckCircle, faEdit, faTrash, faCat, faSyncAlt, faSort, faSortUp, faSortDown, faTh, faList, faWindowClose } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-
 library.add(faPlus, faCheckCircle, faEdit, faTrash, faCat, faSyncAlt, faSort, faSortUp, faSortDown, faTh, faList, faWindowClose);
-
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
