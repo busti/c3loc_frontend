d1279eb09855083836aa7e2bec0be0d2fb14a291
jedi <git@m.j3d1.de>
2019-12-24 16:08:22 +0100

    implement Events view as table

--- src/views/Events.vue
+++ src/views/Events.vue
@@ -1,10 +1,36 @@
 <template>
-    <p>Events</p>
+  <div class="container-fluid px-xl-5 mt-3">
+    <div class="row">
+      <div class="col-xl-8 offset-xl-2">
+        <Table
+          :columns="['slug', 'name']"
+          :items="events"
+          :keyName="'slug'"
+          v-slot="{ item }"
+        >
+          <div class="btn-group">
+            <button class="btn btn-secondary" @click.stop="changeEvent(item)" >
+              <font-awesome-icon icon="archive"/> use
+            </button>
+            <button class="btn btn-danger" @click.stop="" >
+              <font-awesome-icon icon="trash"/> delete
+            </button>
+          </div>
+        </Table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import {mapActions, mapState} from 'vuex';
+import Table from '@/components/Table';
+
 export default {
   name: 'Events',
+  components: {Table},
+  computed: mapState(['events']),
+  methods: mapActions(['changeEvent']),
 };
 </script>
 
