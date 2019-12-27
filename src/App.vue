14d2c05c1fc076bf9437d56ef028d254e370047a
busti <mbust@live.de>
2019-12-27 02:54:31 +0100

    fix toast pointer event pass-through

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
         <AddItemModal v-if="addModalOpen" @close="closeAddModal()" isModal="true"/>
         <Navbar @addClicked="openAddModal()"/>
         <router-view/>
-        <div aria-live="polite" aria-atomic="true" class="d-flex justify-content-end align-items-start fixed-top mx-1 my-5 py-3" style="min-height: 200px; z-index: 100000">
-            <Toast v-for="toast in toasts" :key="toast" :title="toast.title" :message="toast.message" :color="toast.color" @close="removeToast(toast.key)"/>
+        <div aria-live="polite" aria-atomic="true" class="d-flex justify-content-end align-items-start fixed-top mx-1 my-5 py-3" style="min-height: 200px; z-index: 100000; pointer-events: none">
+            <Toast v-for="toast in toasts" :key="toast" :title="toast.title" :message="toast.message" :color="toast.color" @close="removeToast(toast.key)" style="pointer-events: auto"/>
         </div>
     </div>
 </template>
